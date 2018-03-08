0d32692db96567bc07492996b41d6bf0d326faf2
sbm <sbmart@ya.ru>
2018-03-08 17:01:30 +0300

    Added auth in MongoDB

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const messages = require('./messages/messages.service.js');
 const mongomessages = require('./mongomessages/mongomessages.service.js');
+const users = require('./users/users.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(messages);
   app.configure(mongomessages);
+  app.configure(users);
 };
